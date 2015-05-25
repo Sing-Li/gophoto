715bd655095813479081971e6a8c3b60f44e4bdb
Sing Li <sli@makawave.com>
2015-05-24 20:16:09 -0400

    added upload disable, upload/album toggle, and refactoring

---
+++ setenv.sh
@@ -0,0 +1,2 @@
+export V2CREDS='{   "CloudIntegration": {     "auth_url": "https://keystone2.open.ibmcloud.com"  ,  "swift_url": "https://swift2.open.ibmcloud.com/v1/AUTH_a48d23103" ,"sdk_auth_url": "https://keystone2.open.ibmcloud.com" ,   "project": "7c33e699-9412-41681", "region": "dal09",     "credentials": {  "userid": "westmakaha@yahoo.com",  "password": "I"  }  }}'
+echo "Env variable V2CREDS now set to : $V2CREDS"
